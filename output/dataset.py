4e1a5867f9b0d17120f7197b4f7a5880e2d4c64d
Vicente Agüero <vicenteaguero@uc.cl>
2023-07-18 11:56:29 +0200

    Hi Moshood

--- output/dataset.py
+++ output/dataset.py
@@ -2,7 +2,14 @@ from sklearn.datasets import load_iris
 import pandas as pd
 
 
-def prepare_dataset():
+def prepare_dataset() -> pd.DataFrame:
+    """
+    Prepare the Iris dataset.
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the Iris dataset with renamed columns.
+
+    """
     data = pd.DataFrame(load_iris().data, columns=load_iris().feature_names)
     data["target"] = load_iris().target
     data.rename(
@@ -17,15 +24,32 @@ def prepare_dataset():
     return data
 
 
-def subseting_dataset(data):
+def subseting_dataset(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Subset the dataset based on specific conditions.
+
+    Args:
+        data (pd.DataFrame): The input pandas DataFrame containing the dataset.
+
+    Returns:
+        pd.DataFrame: A subset of the input DataFrame that meets the specified conditions.
+
+    """
     data = data[data["sepal_length"] > 5]
     data = data[(data["sepal_width"] < 4) & (data["sepal_width"] > 2.5)]
     data = data[data["petal_length"] > 2]
     data = data[(data["petal_width"] > 0.5) & (data["petal_width"] < 2)]
     return data
 
+def load_dataset() -> pd.DataFrame:
+    """
+    Load and subset the dataset.
 
-dataset = prepare_dataset()
-subset_data = subseting_dataset(data=dataset)
-print(subset_data.head())
-print(subset_data.target.nunique())
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the subset of the Iris dataset.
+    """
+    dataset = prepare_dataset()
+    return subseting_dataset(data=dataset)
+
+if __name__ == "__main__":
+    ...
--- output/pipeline.py
+++ output/pipeline.py
@@ -1,3 +1,6 @@
+from IPython.display import display
+
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -5,6 +8,8 @@ from sklearn.dummy import DummyClassifier
 from sklearn import metrics
 
 import pandas as pd
+
+from dataset import load_dataset
 
 class Pipeline:
 
@@ -18,20 +23,67 @@ class Pipeline:
         self._start()
 
     def _start(self) -> pd.DataFrame:
-        self.models = [eval(model)() for model in self.models]
+        """
+        Start the pipeline
+        """
+        try:
+            self.models = [eval(model)() for model in self.models]
+        except NameError:
+            raise NameError('Model not found')
+        self._split_train_test()
 
-    def split_train_test(self) -> None:
-        ...
+    def _split_train_test(self) -> None:
+        """
+        Split the data into train and test
+        """
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.data.drop(columns=['target']),
+            self.data['target'],
+            test_size=0.2,
+            random_state=42
+        )
 
     def fit(self) -> None:
+        """
+        Fit the models
+        """
         for model in self.models:
             model.fit(self.X_train, self.y_train)
 
     def predict(self) -> pd.DataFrame:
-        ...
+        """
+        Predict the models
+        """
+        self.predictions = {}
+        for model in self.models:
+            self.predictions[model.__class__.__name__] = model.predict(self.X_test)
+        self._compute_scores()
+
+    def _compute_scores(self) -> None:
+        self.scores = dict()
+        for model in self.models:
+            self.scores[model.__class__.__name__] = [metrics.accuracy_score(
+                self.y_test,
+                self.predictions[model.__class__.__name__],
+            )]
 
     def summary(self) -> pd.DataFrame:
-        ...
+        """
+        Summary of the models
+        """
+        display((
+            pd
+            .DataFrame(self.scores, index=['Accuracy Score'])
+            .T
+            .sort_values('Accuracy Score', ascending=False)
+        ))
 
 if __name__ == "__main__":
-    pipe = Pipeline()
+    models = [
+        'RandomForestClassifier', 'KNeighborsClassifier', 'DummyClassifier'
+    ]
+    dataset = load_dataset()
+    pipe = Pipeline(models=models, data=dataset)
+    pipe.fit()
+    pipe.predict()
+    pipe.summary()
